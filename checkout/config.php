aa59138ad41d5fc7cabc7856cfed06afb9b7b2df
Pedro Lucas <143149318+Pedrolhf-dev@users.noreply.github.com>
2025-10-12 18:58:07 -0300

    Delete checkout directory

--- checkout/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('MP_ACCESS_TOKEN', 'APP_USR-7698363442866260-061020-7e3a36b337005f166be8a75dccc08977-1096120387');
-define('MP_WEBHOOK_URL', 'https://lightpink-dogfish-861891.hostingersite.com/checkout/webhook.php');
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'u953537988_futebol');
-define('DB_USER', 'u953537988_futebol');
-define('DB_PASS', '13579012Victor)');
-?>
--- checkout/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erro de conexão: " . $e->getMessage());
-}
-?>
